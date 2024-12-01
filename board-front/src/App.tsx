efc22ef12ff04ec918be26cdfdd938d44e2808a9
minjunsong <songmj5000@naver.com>
2024-12-02 00:20:00 +0900

    commit

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,18 +1,50 @@
-import React, { useState } from 'react';
+import Footer from 'layouts/Footer';
 import './App.css';
-import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
-import Top3Item from 'components/Top3Item';
-import CommentItem from 'components/CommentItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
+import { Route, Routes } from 'react-router-dom';
+import Main from 'views/Main';
+import Authentication from 'views/Authentication';
+import Search from 'views/Search';
+import User from 'views/User';
+import BoardUpdate from 'views/Board/Update';
+import BoardDetail from 'views/Board/Detail';
+import BoardWrite from 'views/Board/Write';
+import Container from 'layouts/Container';
+import { MAIN_PATH } from 'constant';
+import { AUTH_PATH } from 'constant';
+import { USER_PATH } from 'constant';
+import { SEARCH_PATH } from 'constant';
+import { BOARD_PATH } from 'constant';
+import { BOARD_WRITE_PATH } from 'constant';
+import { BOARD_DETAIL_PATH } from 'constant';
+import { BOARD_UPDATE_PATH } from 'constant';
 
+
+//        component: Application 컴포넌트       //
 function App() {
 
-  const [value, setValue] = useState<string>('');
+//        render: Application 컴포넌트 렌터링      //
+// description: 메인 화면 : '/' - Main //
+// description: 로그인 + 회원가입 화면: '/auth' - Authentication //
+// description:  검색 화면 : '/search/:searchWord' - Search //
+// description: 유저 페이지 : 'user/:useremail' - User //
+// description: 게시물 상세보기 : 'board/detail/:boardNumber' - BoardDetail //
+// description: 게시물 작성하기 : 'board/write' - BoardWrite //
+// description: 게시물 수정하기 : 'board/update/:boardNumber' - BoardUpdate //
   return (
-    <>
-      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={true} setValue={setValue} message='aaaa' />
-    </>
+    <Routes>
+      <Route element={<Container />}>
+        <Route path={MAIN_PATH()} element={<Main />} />
+        <Route path={AUTH_PATH()} element={<Authentication />} />
+        <Route path={SEARCH_PATH(':searchWord')} element={<Search />} />
+        <Route path={USER_PATH(':userEmail')} element={<User />} />
+        <Route path={BOARD_PATH()}>
+          <Route path={BOARD_WRITE_PATH()} element={<BoardWrite />} />
+          <Route path={BOARD_DETAIL_PATH(':boardNumber')} element={<BoardDetail />} />
+          <Route path={BOARD_UPDATE_PATH(':boardNumber')} element={<BoardUpdate />} />
+        </Route>
+        <Route path='*' element={<h1>404 Not Found</h1>} />
+      </Route>
+    </Routes>
   );
 }
 
--- board-front/src/types/interface/index.ts
+++ board-front/src/types/interface/index.ts
@@ -1,8 +1,10 @@
+import User from './user.interface';
 import BoardListItem from './board-list-item.interface';
 import CommentListItem from './Comment-list_item.interface';
 import FavoriteListItem from './favorite-list-item.interface';
 
 export type {
+    User,
     BoardListItem,
     CommentListItem,
     FavoriteListItem
